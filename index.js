68fa3d284601c329cc681b38b206f75d0f21f755
raulazotla <89797603+raulazotla@users.noreply.github.com>
2021-09-06 13:00:28 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+funcion getIMC(){
+var peso=parseFloat(document.getElementById('Peso').value);
+var altura =paseFloat(document.getElementById('Altura').value);
+  var imc=peso/(altura*altura);
+  document.getElementById("imc").innerHTML=imc;
+ 
+}
